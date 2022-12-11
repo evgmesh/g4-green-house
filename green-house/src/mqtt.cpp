60fc8db625c17dc8e8d40474bb306ec647de0548
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2022-12-11 12:36:25 +0200

    mqtt.c: new class init

---
+++ green-house/src/mqtt.cpp
@@ -0,0 +1,18 @@
+/*
+ * mqtt.cpp
+ *
+ *  Created on: 11 Dec 2022
+ *      Author: evgenymeshcheryakov
+ */
+
+#include <mqtt.h>
+
+mqtt::mqtt() {
+	// TODO Auto-generated constructor stub
+
+}
+
+mqtt::~mqtt() {
+	// TODO Auto-generated destructor stub
+}
+

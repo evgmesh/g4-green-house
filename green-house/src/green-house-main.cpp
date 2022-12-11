7684d0a6a802fe7e97d17e14d8b18650482f8c57
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2022-12-11 20:45:22 +0200

    mqtt class: connect, publish functionality. dirty version. clean up and improvments needed

--- green-house/src/green-house-main.cpp
+++ green-house/src/green-house-main.cpp
@@ -10,6 +10,19 @@
 
 #include "common_values.h"
 #include "tasks/green-house_tasks.h"
+#include "mqtt.h"
+
+void vMQTTTask (void *pvParameters) {
+	std::string topic("Please, print something!");
+	std::string message("Please, print something!");
+	mqtt mqtt;
+	mqtt.publish(topic, message);
+
+	while(true) {
+		vTaskDelay(1000);
+	}
+}
+
 
 int
 main (void)
@@ -22,13 +35,13 @@ main (void)
 
   heap_monitor_setup ();
 
-  xTaskCreate (vDisplayTask, "LCD", displayTASK_STACKSIZE, NULL,
-               (tskIDLE_PRIORITY + 1UL), EMPTY_TASK_HANDLE);
+//  xTaskCreate (vDisplayTask, "LCD", displayTASK_STACKSIZE, NULL,
+//               (tskIDLE_PRIORITY + 1UL), EMPTY_TASK_HANDLE);
 
-  xTaskCreate (vButtonTask, "Rotary", buttonTASK_STACKSIZE, NULL,
-               buttonTASK_PRIORITY, EMPTY_TASK_HANDLE);
+//  xTaskCreate (vButtonTask, "Rotary", buttonTASK_STACKSIZE, NULL,
+//               buttonTASK_PRIORITY, EMPTY_TASK_HANDLE);
 
-  xTaskCreate (vMQTTDemoTask, "MQTTDemoTask", democonfigDEMO_STACKSIZE, NULL,
+  xTaskCreate (vMQTTTask, "MQTTTask", democonfigDEMO_STACKSIZE, NULL,
                displayTASK_PRIORITY, EMPTY_TASK_HANDLE);
 
   /* Start the scheduler */

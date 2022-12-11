4b8bade0de1981d2c96bb8ba1533af569d20248b
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2022-12-11 12:36:25 +0200

    mqtt.c: new class init

---
+++ green-house/src/mqtt.h
@@ -0,0 +1,17 @@
+/*
+ * mqtt.h
+ *
+ *  Created on: 11 Dec 2022
+ *      Author: evgenymeshcheryakov
+ */
+
+#ifndef MQTT_H_
+#define MQTT_H_
+
+class mqtt {
+public:
+	mqtt();
+	virtual ~mqtt();
+};
+
+#endif /* MQTT_H_ */

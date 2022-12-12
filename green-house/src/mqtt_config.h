53e402c0b909f335ca22f26b336c89b3c790c148
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2022-12-12 19:29:59 +0200

    clean up, remove of dynamic memory allocations.

--- green-house/src/mqtt_config.h
+++ green-house/src/mqtt_config.h
@@ -8,31 +8,16 @@
 #ifndef MQTT_CONFIG_H_
 #define MQTT_CONFIG_H_
 
-/**
- * @brief The MQTT client identifier used in this example.  Each client identifier
- * must be unique so edit as required to ensure no two clients connecting to the
- * same broker use the same client identifier.
- *
- *!!! Please note a #defined constant is used for convenience of demonstration
- *!!! only.  Production devices can use something unique to the device that can
- *!!! be read by software, such as a production serial number, instead of a
- *!!! hard coded constant.
- *
- * #define democonfigCLIENT_IDENTIFIER				"insert here."
- */
-#define CLIENT_IDENTIFIER				"ESP-MQTT-01"
+
+#define CLIENT_IDENTIFIER			"ESP-MQTT-01"
 
 /**
  * @brief The port to use for the demo.
- *
- * #define democonfigMQTT_BROKER_PORT					( insert here. )
  */
-#define MQTT_BROKER_PORT					1883
+#define MQTT_BROKER_PORT		  1883
 
 /*
  * @brief MQTT broker end point to connect to.
-
- * #define democonfigMQTT_BROKER_ENDPOINT	"insert here."
  */
 #define MQTT_BROKER_ENDPOINT	 "192.168.1.254"
 #define WIFI_SSID	    		 "SmartIotMQTT"
--- green-house/src/tasks/task_mqtt.cpp
+++ green-house/src/tasks/task_mqtt.cpp
@@ -1,12 +1,13 @@
+
 #include "green-house_tasks.h"
 
 void vMQTTTask (void *pvParameters) {
 	std::string topic("channels/1955513/publish");
-	std::string message("field1=250&field2=34&field3=22&field4=1&field5=100&created_at=");
+	std::string message("field1=299&field2=299&field3=299&field4=1&field5=90&created_at=");
 	mqtt mqtt;
-	mqtt.publish(topic, message);
-
 	while(true) {
-		vTaskDelay(1000);
+		mqtt.publish(topic, message);
+//		vTaskDelay(1000);
 	}
+//		vTaskDelay(1000);
 }

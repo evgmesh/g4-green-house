f03df2c6dc824772641c66f093d43e23b2748cb5
RedHawk <tadshig@gmail.com>
2022-12-12 13:41:50 +0200

    Sensors: Added co2 and rel hum sensors libraries.

---
+++ green-house/src/srhtHMP60.h
@@ -0,0 +1,35 @@
+#ifndef SRHTHMP60_H_
+#define SRHTHMP60_H_
+
+#include "chip.h"
+#include "modbus/ModbusMaster.h"
+#include "modbus/ModbusRegister.h"
+
+class srhtHMP60 {
+public:
+	srhtHMP60(unsigned int retries = 3, unsigned int wait = 100);
+	virtual ~srhtHMP60();
+	bool read(float &temp, float &rhum, bool retry = true);
+	bool read_temp(float &data, bool retry = true);
+	bool read_rhum(float &data, bool retry = true);
+	bool get_status();
+	unsigned int get_elapsed_time();
+private:
+	ModbusMaster node;
+	ModbusRegister rh0;
+	ModbusRegister rh1;
+	ModbusRegister t0;
+	ModbusRegister t1;
+	ModbusRegister err_reg;
+	ModbusRegister err_code;
+	ModbusRegister err_code2;
+	const unsigned int retries;
+	const unsigned int wait;
+	bool status;
+	unsigned int elapsed_time;
+	bool check_status();
+	int get_error();
+	float binary32_to_float(const unsigned int bin32);
+};
+
+#endif /* SRHTHMP60_H_ */

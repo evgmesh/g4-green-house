d338833b1370001dd04417e8d7972a053a52b8e2
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-12-14 12:56:03 +0200

    mqtt_task: remove peek and add pvparam

--- green-house/src/tasks/task_mqtt.cpp
+++ green-house/src/tasks/task_mqtt.cpp
@@ -1,33 +1,34 @@
 
 #include "green-house_tasks.h"
-#define mqttTOPIC 		"channels/1955513/publish"
-#define mqttMESSAGE 	"field1=%4.0f&field2=%4.1f&field3=%3.1f&field4=%d&field5=%u"
-#define BUFSIZE			100
+#define mqttTOPIC "channels/1955513/publish"
+#define mqttMESSAGE                                                           \
+  "field1=%4.0f&field2=%4.1f&field3=%3.1f&field4=%d&field5=%u"
+#define BUFSIZE 100
 
-void printFormat(char *, int, const char *, ...);
+void printFormat (char *, int, const char *, ...);
 
-void vMQTTTask(void *pvParameters)
+void
+vMQTTTask (void *pvParameters)
 {
-	GH_DATA dataSet;
-	mqtt mqtt;
-	char buffer[BUFSIZE];
-	while (true)
-	{
-		xQueuePeek(sensors_q, &dataSet, portMAX_DELAY);
-		printFormat(buffer, BUFSIZE, mqttMESSAGE,
-					dataSet.co2_val, dataSet.rhum_val,
-					dataSet.temp_val, (int)dataSet.valve_open,
-					dataSet.set_point);
-		std::string message = buffer;
-		mqtt.publish(mqttTOPIC, message);
-		printf("%s\n", message.c_str());
-	}
+  GH_DATA *dataSet = static_cast<GH_DATA *> (pvParameters);
+  mqtt mqtt;
+  char buffer[BUFSIZE];
+  while (true)
+    {
+      printFormat (buffer, BUFSIZE, mqttMESSAGE, dataSet->co2_val,
+                   dataSet->rhum_val, dataSet->temp_val,
+                   (int)dataSet->valve_open, dataSet->set_point);
+      std::string message = buffer;
+      mqtt.publish (mqttTOPIC, message);
+      printf ("%s\n", message.c_str ());
+    }
 }
 
-void printFormat(char *buf, int size, const char *fmt, ...)
+void
+printFormat (char *buf, int size, const char *fmt, ...)
 {
-	va_list args;
-	va_start(args, fmt);
-	vsnprintf(buf, size, fmt, args);
-	va_end(args);
+  va_list args;
+  va_start (args, fmt);
+  vsnprintf (buf, size, fmt, args);
+  va_end (args);
 }

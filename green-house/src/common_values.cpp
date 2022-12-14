2ea69dae33989fa311842fafbe5f34f544c11689
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-12-14 11:02:59 +0200

    rotary_task: delete task and move func

--- green-house/src/common_values.cpp
+++ green-house/src/common_values.cpp
@@ -8,7 +8,7 @@
 #include "common_values.h"
 
 /* global variables */
-QueueHandle_t queue;
+QueueHandle_t action_q;
 QueueHandle_t set_point_queue;
 QueueHandle_t sensors_q;
 extern GH_DATA gh_common = { 0, 0, 0, 0, false };
@@ -16,7 +16,7 @@ extern GH_DATA gh_common = { 0, 0, 0, 0, false };
 void
 create_all_queues()
 {
-  queue = xQueueCreate (100, sizeof (RotaryAction));
+  action_q = xQueueCreate (100, sizeof (RotaryAction));
   set_point_queue = xQueueCreate (1, sizeof (uint32_t));
   sensors_q = xQueueCreate(1, sizeof(GH_DATA));
 }
--- green-house/src/tasks/task_mbsensors.cpp
+++ green-house/src/tasks/task_mbsensors.cpp
@@ -2,39 +2,60 @@
 #include "sco2GMP252.h"
 #include "srhtHMP60.h"
 
+Rotary *rot;
+
+extern "C"
+{
+  void
+  QEI_IRQHandler (void)
+  {
+    portEND_SWITCHING_ISR (rot->rotate_isr ());
+  }
+
+  void
+  PIN_INT1_IRQHandler (void)
+  {
+    portEND_SWITCHING_ISR (rot->press_isr ());
+  }
+}
+
 void
 vMbsensorsTask (void *pvParams)
 {
-    //sensors_q = xQueueCreate(1, sizeof(GH_DATA));
-    GH_DATA data;
-    data.co2_val = 0;
-    data.rhum_val = 0;
-    data.temp_val = 0;
-    sco2GMP252 sco2(0, 10);
-    srhtHMP60 rht(0, 10);
+  // sensors_q = xQueueCreate(1, sizeof(GH_DATA));
+  Rotary rotor (&action_q);
+  rot = &rotor;
+  GH_DATA data;
+  data.co2_val = 0;
+  data.rhum_val = 0;
+  data.temp_val = 0;
+  sco2GMP252 sco2 (0, 10);
+  srhtHMP60 rht (0, 10);
 
-    while (1)
+  while (1)
     {
-        //Read relative humidity.
-        rht.read_rhum(data.rhum_val, false);
-        vTaskDelay(1000);
+      // Read relative humidity.
+      rht.read_rhum (data.rhum_val, false);
+      vTaskDelay (1000);
 
-        //Read temperature.
-        rht.read_temp(data.temp_val, false);
-        vTaskDelay(1000);
+      // Read temperature.
+      rht.read_temp (data.temp_val, false);
+      vTaskDelay (1000);
 
-        //Read co2.
-        #if ARDUINO_SIM
-        //Use imprecise read.
-        sco2.read_rapid(data.co2_val, false);
-        #else
-        //Use precise read.
-        sco2.read(data.co2_val, data.rhum_val, false);
-        #endif
+// Read co2.
+#if ARDUINO_SIM
+      // Use imprecise read.
+      sco2.read_rapid (data.co2_val, false);
+#else
+      // Use precise read.
+      sco2.read (data.co2_val, data.rhum_val, false);
+#endif
 
-        xQueueOverwrite(sensors_q, (void *)&data);
+      xQueueOverwrite (sensors_q, (void *)&data);
 
-        printf("[SENSORS] Sensors data:\r\nco2: %.2f\r\nrel hum: %.2f\r\ntemp: %.2f\r\n", data.co2_val, data.rhum_val, data.temp_val);
-        vTaskDelay(1000);
+      printf ("[SENSORS] Sensors data:\r\nco2: %.2f\r\nrel hum: %.2f\r\ntemp: "
+              "%.2f\r\n",
+              data.co2_val, data.rhum_val, data.temp_val);
+      vTaskDelay (1000);
     }
 }

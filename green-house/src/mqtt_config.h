71da48f64c1edd76f7ee2c3e190831f942a6d98b
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2022-12-16 10:20:57 +0200

    mqtt_config: network parameters cleanup

--- green-house/src/mqtt_config.h
+++ green-house/src/mqtt_config.h
@@ -8,38 +8,23 @@
 #ifndef MQTT_CONFIG_H_
 #define MQTT_CONFIG_H_
 
-#define SMARTIOTMQTT 1
-#define DANIIL 0
 #define GREENHOUSEDEMO 0
 
-/**
- * @brief The port to use for the demo.
- */
 #define MQTT_BROKER_PORT 1883
-#define WIFI_SSID "SmartIotMQTT"
-#define WIFI_PASS "SmartIot"
-/*
- * @brief MQTT broker end point to connect to.
- */
-#if SMARTIOTMQTT
-#define CLIENT_IDENTIFIER "GH-MQTT-G4"
-#define MQTT_BROKER_ENDPOINT "192.168.1.136"
-#define BROKER_USER_NAME "group4"
-#define BROKER_PASSWORD "12345678"
+#define WIFI_SSID "Network SSID"
+#define WIFI_PASS "Network Password"
 
-#elif DANIIL
-#define CLIENT_IDENTIFIER "GH-MQTT-G4"
-#define MQTT_BROKER_ENDPOINT "192.168.43.1"
-#define WIFI_SSID "test"
-#define WIFI_PASS "12345678"
-#define BROKER_USER_NAME "group4"
-#define BROKER_PASSWORD "12345678"
-
-#elif GREENHOUSEDEMO
+#if GREENHOUSEDEMO
 #define BROKER_USER_NAME "DCAmDzgFFhoKKy8kCBw3NQA"
 #define BROKER_PASSWORD "qKxqzEzD+xMf2LMg0SU24WYk"
 #define CLIENT_IDENTIFIER "DCAmDzgFFhoKKy8kCBw3NQA"
 #define MQTT_BROKER_ENDPOINT "mqtt3.thingspeak.com"
+
+#else
+#define BROKER_USER_NAME "username"
+#define BROKER_PASSWORD "password"
+#define CLIENT_IDENTIFIER "clientID"
+#define MQTT_BROKER_ENDPOINT "mqtt3.example.com"
 
 #endif
 
